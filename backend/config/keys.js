abc52c2df3a0b6a15d2187f7f48a137593c85a72
Merrily Tan <merrilytan@gmail.com>
2019-06-11 08:24:53 -0400

    cleaned up data.json & unnecessary files

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const DB_HOST = process.env.DB_HOST
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD= process.env.DB_PASSWORD
+
 module.exports = {
     MongoURI: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}?retryWrites=true`
-    
 }
